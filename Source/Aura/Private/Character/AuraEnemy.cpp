4bdf05238b1cdd5fb4a7e8fdccae17212340656b
Ale Pereira <naitwa-alexandre@gmail.com>
2025-11-23 20:20:26 -0300

    creates ability system component and sets it to player and enemies

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -15,18 +15,17 @@ AAuraEnemy::AAuraEnemy()
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 	
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
 }
 
 void AAuraEnemy::HightlightActor()
 {
-	if (Weapon)
-	{
-		GetMesh()->SetRenderCustomDepth(true);
-		GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	check(Weapon);
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 
-		Weapon->SetRenderCustomDepth(true);
-		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
-	}
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHightlightActor()
@@ -34,4 +33,12 @@ void AAuraEnemy::UnHightlightActor()
 	GetMesh()->SetRenderCustomDepth(false);
 
 	Weapon->SetRenderCustomDepth(false);
+}
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(AbilitySystemComponent);
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
