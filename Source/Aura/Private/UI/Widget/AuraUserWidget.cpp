965fb1c8b45f22dc9dbbc82f6b9d56f5d2297979
Ale Pereira <naitwa-alexandre@gmail.com>
2025-11-25 08:48:01 -0300

    User widget and widget controller created

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Blacksheep1337
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
