fda93ab0155c6d1600d0cf60b51d8c9f7a068ab0
Ale Pereira <naitwa-alexandre@gmail.com>
2025-12-03 15:33:02 -0300

    wip: attribute info dataset created tags now handled in c++

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -14,18 +14,20 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	 */
 	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Primary.Strength"), FString("Increases physical damage."));
-	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get()
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Primary.Intelligence"), FString("Increases magical damage."));
 	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Primary.Resilience"), FString("Increases Armor and Armor Penetration."));
 	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Primary.Vigor"), FString("Increases Health."));
-	
 	/**
 	 * Secondary Attributes
 	 */
 	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves block chance."));
+	
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get()
+	.AddNativeGameplayTag(FName("Attributes.Secondary.ArmorPenetration"), FString("Ignores Percentage of enemy Armor, increases Critical Hit Chance."));
 	
 	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get()
 		.AddNativeGameplayTag(FName("Attributes.Secondary.MaxHealth"), FString("Maximum amount of Health obtainable."));
