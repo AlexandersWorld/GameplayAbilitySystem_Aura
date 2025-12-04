cb12a28cf39be74d891049a7507bb5baf93a5553
Ale Pereira <naitwa-alexandre@gmail.com>
2025-12-04 08:50:45 -0300

    wip: Attribute info delecate broadecasting initial values

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,10 +3,23 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+	
+	check(AttributeInfo);
+	
+	FAuraAttributeInfo AuraAttributeInfo = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	
+	AuraAttributeInfo.AttributeValue = AuraAttributeSet->GetStrength();
+	
+	AttributeInfoDelegate.Broadcast(AuraAttributeInfo);
 }
